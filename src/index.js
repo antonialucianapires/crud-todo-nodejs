e3ea00644928bd556662b8cd9f9a16f316e4231c
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-11-06 17:16:50 -0300

    feat: atualizar tarefa

--- src/index.js
+++ src/index.js
@@ -65,7 +65,20 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ erro: "todo not found" });
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
